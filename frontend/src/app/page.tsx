ee8635fd41fa37b0097bd65b8e4fc9b2ccad3cf6
Ravalkrushn <ravalkrishna0313@gmail.com>
2026-03-06 17:15:55 +0530

    file stu. fix

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,13 @@
 import MainNavbar from "@/components/Navbar/MainNavbar";
 import HeroSection from "@/components/HomePage/LandingPage";
-import DoctorSection from "@/components/sections/DoctorSection";
-import ComprehensiveServicesSection from "@/components/sections/ComprehensiveServicesSection";
-import AboutSection from "@/components/sections/AboutSection";
-import BeforeAfterGallery from "@/components/gallery/BeforeAfterGallery";
-import ReviewsSection from "@/components/sections/ReviewsSection";
+import DoctorSection from "@/components/HomePage/AboutDocter/DoctorSection";
+import ComprehensiveServicesSection from "@/components/HomePage/DentalServices/ComprehensiveServicesSection";
+import AboutSection from "@/components/HomePage/ChooseUs/AboutSection";
+import BeforeAfterGallery from "@/components/HomePage/Before&AfterGallery/BeforeAfterGallery";
+import ReviewsSection from "@/components/HomePage/Testimonials/ReviewsSection";
 import ServicesSection from "@/components/sections/ServicesSection";
 import TestimonialsSection from "@/components/sections/TestimonialsSection";
-import LocationSection from "@/components/sections/LocationSection";
+import LocationSection from "@/components/HomePage/ClinicTiming&ContactInfo/LocationSection";
 import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
